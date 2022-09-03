097f98a5d526a191a3b7aca6db72e432d1d60490
Dion Amago <dion@transition9.com>
2022-09-03 11:09:42 -0700

    Do not print server for production builds

--- app/browser/src/config.tsx
+++ app/browser/src/config.tsx
@@ -7,6 +7,4 @@ if (import.meta.env.VITE_SERVER_ORIGIN) {
     }
 }
 
-console.log(`SERVER_ORIGIN=${serverOrigin}`);
-
 export const websocketConnectionUrl = `${serverOrigin.replace("http", "ws")}${serverOrigin.endsWith("/") ? "" : "/"}browser/`;
